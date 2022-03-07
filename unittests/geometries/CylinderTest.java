51ad98aeaf9bd0cc04d6f0a25fb1250119b3b77d
Asaf Bigel <bigel@g.jct.ac.il>
2022-03-07 16:49:41 +0200

    Add test functions

--- unittests/geometries/CylinderTest.java
+++ unittests/geometries/CylinderTest.java
@@ -1,6 +1,9 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +11,18 @@ class CylinderTest {
 
     @Test
     void getNormal() {
+        Cylinder c = new Cylinder(new Ray(new Point(0,0,0),new Vector(1,0,0)), 1 , 2);
+        // ============ Equivalence Partitions Tests ==============
+        Vector normal1 = c.getNormal(new Point(1,1,0));
+        assertTrue(normal1.equals(new Vector(0,1,0)), "Wrong tube normal");
+        Vector normal2 = c.getNormal(new Point(3,0.5,0));
+        assertTrue(normal1.equals(new Vector(1,0,0)), "Wrong tube normal");
+        Vector normal3 = c.getNormal(new Point(-3,0.5,0));
+        assertTrue(normal1.equals(new Vector(-1,0,0)), "Wrong tube normal");
+        // =============== Boundary Values Tests ==================
+        Vector normal4 = c.getNormal(new Point(3,0,0));
+        assertTrue(normal1.equals(new Vector(1,0,0)), "Wrong tube normal");
+        Vector normal5 = c.getNormal(new Point(-3,0,0));
+        assertTrue(normal1.equals(new Vector(-1,0,0)), "Wrong tube normal");
     }
 }
--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -2,15 +2,18 @@ package primitives;
 
 import org.junit.jupiter.api.Test;
 
+import static java.lang.System.out;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointTest {
-
+    Point p1 = new Point(1, 2, 3);
     @Test
     void add() {
+        assertTrue (p1.add(new Vector(-1, -2, -3)).equals(new Point(0, 0, 0)),"ERROR: Point + Vector does not work correctly");
     }
 
     @Test
     void subtract() {
+        assertTrue (new Vector(1, 1, 1).equals(new Point(2, 3, 4).subtract(p1)),"ERROR: Point - Point does not work correctly");
     }
 }
