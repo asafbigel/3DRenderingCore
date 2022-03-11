a652d6e0b26219265aaec2d49a21cfdc639e8b84
Asaf Bigel <bigel@g.jct.ac.il>
2022-03-11 13:24:13 +0200

    Add test functions

--- unittests/primitives/PointTest.java
+++ unittests/primitives/PointTest.java
@@ -9,11 +9,13 @@ class PointTest {
     Point p1 = new Point(1, 2, 3);
     @Test
     void add() {
-        assertTrue (p1.add(new Vector(-1, -2, -3)).equals(new Point(0, 0, 0)),"ERROR: Point + Vector does not work correctly");
+      //  assertTrue (p1.add(new Vector(-1, -2, -3)).equals(new Point(0, 0, 0)),"ERROR: Point + Vector does not work correctly");
+        assertEquals(p1.add(new Vector(-1, -2, -3)),new Point(0, 0, 0),"ERROR: Point + Vector does not work correctly");
     }
 
     @Test
     void subtract() {
-        assertTrue (new Vector(1, 1, 1).equals(new Point(2, 3, 4).subtract(p1)),"ERROR: Point - Point does not work correctly");
+       // assertTrue (new Vector(1, 1, 1).equals(new Point(2, 3, 4).subtract(p1)),"ERROR: Point - Point does not work correctly");
+        assertEquals (new Vector(1, 1, 1), new Point(2, 3, 4).subtract(p1),"ERROR: Point - Point does not work correctly");
     }
 }
